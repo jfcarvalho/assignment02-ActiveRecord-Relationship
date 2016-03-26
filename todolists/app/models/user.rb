9dcfe6a7a74e483dd6b32dbcbc338ed5529be068
jfcarvalho <jfcarvalho@dcc.ufba.br>
2016-03-26 20:15:32 -0300

    Finally rq08 has been done

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_one :profile
 	has_many :todo_lists
-	has_many :anything, through: :todo_items, source: :todo_lists 
+	has_many :todo_items, through: :todo_lists, source: :todo_items 
 end
