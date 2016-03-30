0a3bf819c7065e8b12e3eb149b5a7d788470ff0e
jfcarvalho <jfcarvalho@dcc.ufba.br>
2016-03-30 14:04:59 -0300

    rq11 funcionando

--- todolists/app/models/profile.rb
+++ todolists/app/models/profile.rb
@@ -1,3 +1,21 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validate :notbothnull
+  validates :gender, inclusion: { in: %w(male female)}
+  validate :sue_male
+
+  def notbothnull
+		if self.first_name.nil? and self.last_name.nil?  
+			errors.add :last_name, "Error"
+		end
+
+	end
+
+ def sue_male
+		if self.gender.eql? "male" and self.first_name.eql? "Sue"  
+			errors.add :first_name, "Error"
+		end
+
+		end
+
 end
